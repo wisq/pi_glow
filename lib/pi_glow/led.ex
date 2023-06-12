8a14c0b929c982f7c623608ae6d7c666ffbf50fe
Adrian Irving-Beer <wisq@wisq.net>
2023-06-12 14:35:24 -0400

    I like the Python library's "ring" better than "radius".
    (Still prefer "arm" over "leg" though.  It's supposed to be kinda like a
    spiral nebula after all.)

--- lib/pi_glow/led.ex
+++ lib/pi_glow/led.ex
@@ -1,37 +1,37 @@
 defmodule PiGlow.LED do
   alias __MODULE__
 
-  @enforce_keys [:index, :arm, :colour, :radius]
+  @enforce_keys [:index, :arm, :colour, :ring]
   defstruct(
     index: nil,
     arm: nil,
-    radius: nil,
+    ring: nil,
     colour: nil
   )
 
   def leds do
     [
       # Top arm:
-      %LED{index: 0x0A, arm: 1, radius: 1, colour: :white},
-      %LED{index: 0x05, arm: 1, radius: 2, colour: :blue},
-      %LED{index: 0x06, arm: 1, radius: 3, colour: :green},
-      %LED{index: 0x09, arm: 1, radius: 4, colour: :amber},
-      %LED{index: 0x08, arm: 1, radius: 5, colour: :orange},
-      %LED{index: 0x07, arm: 1, radius: 6, colour: :red},
+      %LED{index: 0x0A, arm: 1, ring: 1, colour: :white},
+      %LED{index: 0x05, arm: 1, ring: 2, colour: :blue},
+      %LED{index: 0x06, arm: 1, ring: 3, colour: :green},
+      %LED{index: 0x09, arm: 1, ring: 4, colour: :amber},
+      %LED{index: 0x08, arm: 1, ring: 5, colour: :orange},
+      %LED{index: 0x07, arm: 1, ring: 6, colour: :red},
       # Right arm:
-      %LED{index: 0x0B, arm: 2, radius: 1, colour: :white},
-      %LED{index: 0x0C, arm: 2, radius: 2, colour: :blue},
-      %LED{index: 0x0E, arm: 2, radius: 3, colour: :green},
-      %LED{index: 0x10, arm: 2, radius: 4, colour: :amber},
-      %LED{index: 0x11, arm: 2, radius: 5, colour: :orange},
-      %LED{index: 0x12, arm: 2, radius: 6, colour: :red},
+      %LED{index: 0x0B, arm: 2, ring: 1, colour: :white},
+      %LED{index: 0x0C, arm: 2, ring: 2, colour: :blue},
+      %LED{index: 0x0E, arm: 2, ring: 3, colour: :green},
+      %LED{index: 0x10, arm: 2, ring: 4, colour: :amber},
+      %LED{index: 0x11, arm: 2, ring: 5, colour: :orange},
+      %LED{index: 0x12, arm: 2, ring: 6, colour: :red},
       # Left arm:
-      %LED{index: 0x0D, arm: 3, radius: 1, colour: :white},
-      %LED{index: 0x0F, arm: 3, radius: 2, colour: :blue},
-      %LED{index: 0x04, arm: 3, radius: 3, colour: :green},
-      %LED{index: 0x03, arm: 3, radius: 4, colour: :amber},
-      %LED{index: 0x02, arm: 3, radius: 5, colour: :orange},
-      %LED{index: 0x01, arm: 3, radius: 6, colour: :red}
+      %LED{index: 0x0D, arm: 3, ring: 1, colour: :white},
+      %LED{index: 0x0F, arm: 3, ring: 2, colour: :blue},
+      %LED{index: 0x04, arm: 3, ring: 3, colour: :green},
+      %LED{index: 0x03, arm: 3, ring: 4, colour: :amber},
+      %LED{index: 0x02, arm: 3, ring: 5, colour: :orange},
+      %LED{index: 0x01, arm: 3, ring: 6, colour: :red}
     ]
   end
 
